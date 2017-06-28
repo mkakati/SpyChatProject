049de68dedfc97617568f453c35404d1baa5111f
mkakati <mrigangakakati@gmail.com>
2017-06-28 12:06:23 -0700

    SpyChat project

---
+++ .idea/spychat3.py
@@ -0,0 +1,9 @@
+n=raw_input("enter ur name")
+if n=="Rajat":
+    print("Hi!",n)
+if n=="Abinav":
+    print("Bye!",n)
+else:
+    print("You are an idiot")
+
+
---
+++ .idea/spychat4.py
@@ -0,0 +1,14 @@
+name=raw_input("what is ur name?")
+def kakati():
+    age=int(raw_input("what is your age"))
+    age=age+20
+    print("Your age is "+str(age))
+if name=="Rajat":
+    kakati()
+elif name=="Abhinav":
+    kakati()
+
+
+
+
+
